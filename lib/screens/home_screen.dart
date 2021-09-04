276aeb2eacbe87ed29860bb233b8ac2ebedf1bbd
Garima Bisht <bisht.garima01@gmail.com>
2021-09-04 12:57:45 +0530

    background image added

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -6,32 +6,56 @@ import 'package:weather_app/widgets/tiles_widget.dart';
 import 'package:intl/intl.dart';
 
 class HomeScreen extends StatefulWidget {
+  final List AllLocationWeather;
+  final Map currentLocationWeather;
 
-  final List LocationWeather;
-  final Map currentWeather;
-
-  HomeScreen( this.LocationWeather, this.currentWeather);
+  HomeScreen(this.AllLocationWeather, this.currentLocationWeather);
 
   @override
   _HomeScreenState createState() => _HomeScreenState();
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-   String _timeString="";
+  String _timeString = "";
+  String currentWeather = '';
+  late int temperature;
+  String weatherIcon = '';
+  String place = '';
+
+  void updateUI(var weather) {
+    setState(() {
+      if (currentWeather == null) {
+        temperature = 0;
+        weatherIcon = 'Error';
+        place = '';
+        currentWeather = 'Unable to get weather data';
+        return;
+      }
+      double temp =weather['main']['temp'];
+      temperature = temp.toInt();
+      weatherIcon = WeatherModel().getWeatherIcon(temperature);
+      currentWeather = weather['weather'][0]['main'];
+      place = weather['name'];
+    });
+  }
 
   void _getTime() {
     final String formattedDateTime =
-    DateFormat('MM dd yyyy \n kk:mm').format(DateTime.now()).toString();
+        DateFormat('MM dd yyyy \n kk:mm').format(DateTime.now()).toString();
     setState(() {
       _timeString = formattedDateTime;
     });
   }
+
   @override
   void initState() {
     // TODO: implement initState
     super.initState();
     Timer.periodic(Duration(seconds: 1), (Timer t) => _getTime());
+    updateUI(widget.currentLocationWeather);
+
   }
+
   @override
   Widget build(BuildContext context) {
     final width = MediaQuery.of(context).size.width;
@@ -44,35 +68,64 @@ class _HomeScreenState extends State<HomeScreen> {
       body: Stack(
         children: [
           Container(
-            color: Colors.blueGrey,
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage("assets/images/dark_background.png"),
+                fit: BoxFit.cover
+              )
+            ),
+           // color: Colors.blueGrey.shade200,
             width: double.maxFinite,
             child: Center(
               child: Column(
                 crossAxisAlignment: CrossAxisAlignment.center,
-               //mainAxisAlignment: MainAxisAlignment.center,
+                //mainAxisAlignment: MainAxisAlignment.center,
                 mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                 children: [
                   SizedBox(
-                    height: width/5,
+                    height: width / 5,
                   ),
                   Text(
-                    widget.currentWeather['name'],
+                    place,
                     textAlign: TextAlign.center,
-                    style: TextStyle(fontSize: 12,color: Colors.white),
+                    style: TextStyle(fontSize: 12, color: Colors.black),
                   ),
-                  Text(widget.currentWeather['weather'][0]['main'],style: TextStyle(fontSize: 30,color: Colors.white),),
-
+                  Text(
+                    currentWeather,
+                    style: TextStyle(fontSize: 30, color: Colors.white),
+                  ),
                   Padding(
                     padding: const EdgeInsets.all(4.0),
-                    child: Text(widget.currentWeather['main']['temp'].toInt().toString()+"°C",style: TextStyle(fontSize: 15,color: Colors.white),),
+                    child: Text(
+                      temperature.toString() + "°C",
+                      style: TextStyle(fontSize: 15, color: Colors.white),
+                    ),
                   ),
                   Padding(
                     padding: const EdgeInsets.all(8.0),
-                    child: Text(WeatherModel().getWeatherIcon(widget.currentWeather['main']['temp'].toInt())),
+                    child: Text(weatherIcon),
                   ),
-                  Text(_timeString.toString(),style: TextStyle(fontSize: 17,color: Colors.white),textAlign: TextAlign.center,),
-                  SizedBox(
-                    height: width/1,
+                  Text(
+                    _timeString.toString(),
+                    style: TextStyle(fontSize: 17, color: Colors.white),
+                    textAlign: TextAlign.center,
+                  ),
+                  IconButton(
+                    visualDensity: VisualDensity.comfortable,
+                    splashRadius: 25,
+                    splashColor: Colors.white,
+                    iconSize: 25,
+                    hoverColor: Colors.white,
+                      onPressed: () async {
+                        var weatherData =
+                            await WeatherModel().getCurrentWeather();
+                        updateUI(weatherData);
+                      },
+                      icon: Icon(Icons.refresh,color: Colors.white,)),
+                  Expanded(
+                    child: SizedBox(
+                      height: width / 1,
+                    ),
                   )
                 ],
               ),
@@ -84,12 +137,15 @@ class _HomeScreenState extends State<HomeScreen> {
               Padding(
                 padding: const EdgeInsets.all(15.0),
                 child: Container(
-                  height: width/1.8,
+                  height: width / 1.8,
                   child: ListView.builder(
-                    scrollDirection:Axis.horizontal ,
-                    itemCount: widget.LocationWeather.length-1,
+                    scrollDirection: Axis.horizontal,
+                    itemCount: widget.AllLocationWeather.length - 1,
                     itemBuilder: (BuildContext, index) {
-                      return TilesWidget(data: widget.LocationWeather[index+1], time: _timeString.toString(),);
+                      return TilesWidget(
+                        data: widget.AllLocationWeather[index + 1],
+                        time: _timeString.toString(),
+                      );
                     },
                   ),
                 ),
